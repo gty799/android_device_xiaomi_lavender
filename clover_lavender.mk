2225fb106ddcb136c9d6d8fbdea05a158299034a
mitmac29 <118977337+mitmac29@users.noreply.github.com>
2025-07-23 16:46:27 +0530

    Update and rename lineage_lavender.mk to clover_lavender.mk

--- clover_lavender.mk
+++ clover_lavender.mk
@@ -25,7 +25,7 @@
 $(call inherit-product, device/xiaomi/lavender/device.mk)
 
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/clover/config/common_full_phone.mk)
 
 # Build Description
 PRODUCT_BUILD_PROP_OVERRIDES += \
@@ -33,16 +33,13 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildFingerprint=xiaomi/lavender/lavender:10/QKQ1.190910.002/V12.5.3.0.QFGCNXM:user/release-keys \
     DeviceProduct=lavender
 
-MISTOS_MAINTAINER="MiTESH"
-TARGET_ENABLE_BLUR := true
+TARGET_ENABLE_BLUR := false
 TARGET_BUILD_DEVICE_AS_WEBCAM := true
-TARGET_SUPPORTS_64_BIT_APPS := true
-#WITH_GMS := true
-#TARGET_USES_MINI_GAPPS := true
-TARGET_DEFAULT_PIXEL_LAUNCHER := false
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_QUICK_TAP := true
 
 # Device identifier
-PRODUCT_NAME := lineage_lavender
+PRODUCT_NAME := clover_lavender
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_PLATFORM := SDM660
 PRODUCT_DEVICE := lavender
