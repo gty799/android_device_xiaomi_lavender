c21ac093e82ee6833cd2647570efe8d96368816f
gty799 <59225496+gty799@users.noreply.github.com>
2025-08-07 11:13:45 +0300

    Update lineage_lavender.mk

--- lineage_lavender.mk
+++ lineage_lavender.mk
@@ -33,14 +33,15 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildFingerprint=xiaomi/lavender/lavender:10/QKQ1.190910.002/V12.5.3.0.QFGCNXM:user/release-keys \
     DeviceProduct=lavender
 
-TARGET_ENABLE_BLUR := true
+# TARGET_ENABLE_BLUR := true
 TARGET_BUILD_DEVICE_AS_WEBCAM := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_BOOT_ANIMATION_RES := 1080
-WITH_GMS := false
-WITH_BCR := false
-ro.paranoid.maintainer=Timofeyvv8
+WITH_GMS := true
+BUILD_BCR := true
+EVO_BUILD_TYPE := Unofficial
+# ro.paranoid.maintainer=Timofeyvv8
 
 # Device identifier
 PRODUCT_NAME := lineage_lavender
